4ec6bd3157721e19009f664edaca28530b6015fb
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-02-19 10:04:55 -0600

    Reorganized middleware.

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
-const csrf = require('csurf');
 
-const mainRouter = require('./routes/mainRouter');
+const mainRouter = require('./src/routes/mainRouter');
 
 const port = 8000
 
 app.use(cookieParser());    // Obtener cookies
 app.use(express.json());    // Obtener request.body
-app.use('/', mainRouter);
 
-app.all("*", (req, res) => {
-    res.send("Error, ruta no encontrada.");
-})
+app.use('/', mainRouter);
 
 
 
---
+++ src/routes/mainRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mainRouter = express.Router();
+
+const registro = require('./registro');
+
+
+mainRouter.use("/registro", registro);
+
+// define endpoint files
+
+// mainRouter.use("endpoint", endpoint file)
+
+
+
+mainRouter.get('/', (req, res) => {
+    res.send("Hola mundo, estas en inicio.");
+})
+
+app.all("*", (req, res) => {
+    res.send("Error, ruta no encontrada.");
+})
+
+
+module.exports = mainRouter;

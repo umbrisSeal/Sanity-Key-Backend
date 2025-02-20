84a3cd2777329997365cc560b06ccaa5ec3f3f07
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-02-20 16:28:02 -0600

    Removed nodemail due to integration problems.
     Due to many "free" services are actually not free I uninstalled this library and remove this feature from the application.
    Google provides another way to use this library but involves setting an OAuth client, which is out of the scope of this project. Maybe I'll implement it in future projects.

--- src/services/mail/nodemailerConfiguracion.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require("nodemailer");
-
-const nodemailerTransporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: `${process.env.USUARIO_CUENTA_GMAIL}`,
-        pass: `${process.env.PASSWORD_CUENTA_GMAIL}`
-    }
-});

b3a8b50da7a52a47ec7c471216a7dfb2bdd04113
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-02-20 12:38:30 -0600

    Added .env file.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+require("dotenv").config();
 
 const mainRouter = require('./src/routes/mainRouter');
 

3e699ffe8a0ced477bb5b8d7583d3257e96a543c
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-02-20 23:19:11 -0600

    Added new error messages.

--- src/utils/messages/errors/error.js
+++ src/utils/messages/errors/error.js
@@ -4,7 +4,9 @@ const ERROR = {
         EMAIL_INCORRECTO: "El email tiene un formato incorrecto.",
         PASSWORD_FORMATO_INCORRECTO: "La contraseña debe de ser de entre 6 y 20 caracteres de longitud y debe tener al menos: 1 letra mayuscula, 1 numero y 1 caracter especial.",
         PASSWORDS_NO_COINCIDEN: "Las contraseñas no coinciden.",
-        FORMULARIO_INCORRECTO: "El formulario tiene campos incorrectos."
+        FORMULARIO_INCORRECTO: "El formulario tiene campos incorrectos.",
+        USUARIO_REPETIDO: "Este usuario ya existe.",
+        EMAIL_REPETIDO: "El email que intenta registrar ya esta en uso por alguien mas. Por favor, use otro correo electronico."
     },
     en: {
         //

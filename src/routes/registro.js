e8f33835fed281384d84ac1752275cf1e56c8683
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-02-21 13:49:51 -0600

    Fixed: Showing CSRF error when needed.

--- src/routes/registro.js
+++ src/routes/registro.js
@@ -3,6 +3,8 @@ const registro = express.Router();
 
 const csurf = require('csurf');
 const csrfProtection = csurf({cookie: true});
+const ERROR = require('../utils/messages/errors/error');
+
 
 const solicitarCSRFController = require('../controllers/solicitarCSRFController');
 const nuevoRegistroController = require('../controllers/nuevoRegistroController');
@@ -14,8 +16,12 @@ registro.post("/", csrfProtection, nuevoRegistroController);
 
 registro.use( (err, req, res, next) => {
     if(err.code === 'EBADCSRFTOKEN') {
-        // Se requiere una respuesta menos generica.
-        res.send("Tu token CSRF es incorrecto.");
+        res.status(403).json({
+            error: 'CSRF Token',
+            detalles: {
+                _csrf: ERROR.es.CSRF_INVALIDO
+            }
+        });
         return;
     }
     res.send("Algo esta mal, pero no es el error CSRF.");
--- src/utils/messages/errors/error.js
+++ src/utils/messages/errors/error.js
@@ -7,7 +7,8 @@ const ERROR = {
         FORMULARIO_INCORRECTO: "El formulario tiene campos incorrectos.",
         USUARIO_REPETIDO: "Este usuario ya existe.",
         EMAIL_REPETIDO: "El email que intenta registrar ya esta en uso por alguien mas. Por favor, use otro correo electronico.",
-        USUARIO_NO_CREADO: "El usuario no pudo ser registrado. Intentelo mas tarde."
+        USUARIO_NO_CREADO: "El usuario no pudo ser registrado. Intentelo mas tarde.",
+        CSRF_INVALIDO: "El token CSRF esta ausente o es incorrecto."
     },
     en: {
         //
